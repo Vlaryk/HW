2687c234ebd1cc2d6016564911f869db93f2b32d
Vladislav <i am Valadislav.com>
2023-12-03 12:26:08 +0300

    second_save

--- HW.py
+++ HW.py
@@ -1,7 +1,20 @@
-import pandas as pd
 import random
+import pandas as pd
+
 lst = ['robot'] * 10
 lst += ['human'] * 10
+is_robot = []
+is_human = []
+
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
-data.head()
+
+for el in lst:
+    if el == 'robot':
+        is_robot.append(True)
+        is_human.append(False)
+    else:
+        is_robot.append(False)
+        is_human.append(True)
+
+data = pd.DataFrame({'robot': is_robot,'human': is_human})
+print(data.head())
